7d58f1a002311085b3ebf6e3939157dd6fbb6c85
hodatcc@gmail.com <hodatcc@gmail.com>
2025-12-13 18:56:36 +0700

    Fix: Corrected NextAuth export in middleware.ts and updated dependencies/scripts.

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { authConfig } from "./auth.config";
 
 export const { auth: middleware } = NextAuth(authConfig);
 
-//configure which paths the middleware should run on
 
 export const config = {
   matcher: ["/admin/:path*", "/user/:path*", "/appointments/:path*"],
